ee01378a8ec7d59a8e1bd9918118ad6f5501bfdf
Gursewak <samambarsarwala08@gmail.com>
2026-04-18 18:14:14 -0700

    Add files via upload

---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+export const lightTheme = {
+  bg: "#eaeaea",
+  text: "#111",
+  header: "#000",
+  accent: "#2fd3c6"
+};
+
+export const darkTheme = {
+  bg: "#2f2f2f",
+  text: "#fff",
+  header: "#ddd",
+  accent: "#2fd3c6"
+};
